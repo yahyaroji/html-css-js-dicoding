499c44aeedfbbb6ce7b06e8031374b179ed6dd06
Ahmad Fatchur Roji Yahya <yahyaroji9@gmail.com>
2022-11-06 17:19:34 +0700

    first commit

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+function styleImg(x) {
+  x.style.width = "450px";
+  x.style.transform = "rotate(9deg)";
+  x.style.transitionDuration = "1s";
+}
+
+function normalImg(x) {
+  x.style.width = "400px";
+  x.style.transform = "rotate(0deg)";
+}
